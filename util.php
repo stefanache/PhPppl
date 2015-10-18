852cfd211fe3d2299405ec1156666db42554cca3
stefanache <ion.stefanache@yahoo.com>
2015-10-18 12:29:17 +0300

    Create util.php

---
+++ util.php
@@ -0,0 +1,12 @@
+<?php
+  $max_mtRandom=mt_getrandmax();
+  function mtRandom($int_min=0,$int_max=$max_mtRandom){
+    /*
+      Generate a better random integer value
+      Algorithm Mersenne Twister
+      */
+    $int_result=mt_rand($int_min,$int_max);
+    return $int_result; 
+  }
+ 
+?>
