f140bbed87d858937954aac719e9f909ac1a0073
stefanache <ion.stefanache@yahoo.com>
2015-10-18 15:30:32 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,13 @@
-<?php	  
+<?php
+	  //	http://contempo-web.ro/PhPppl
 	  require_once('Mersenne_Twister.php');
-    $mt_obj=new class_MT();
-    $mt_rng=$mt_obj->intMT_rng;
-    echo 'Mersenne Twister RNG = '.$mt_rng.';<br/>';
-  ?>
+          $mt_obj=new class_MT();
+          $mt_rng=$mt_obj->intMT_rng;
+          echo 'Mersenne Twister RNG = '.$mt_rng.';<br/>';
+          //
+          require_once('LibC_rand.php');
+          $libCrand_obj=new class_LibCrand();
+          $libCrand_rng=$libCrand_obj->intLibC_rng;
+          echo 'LibC rng = '.$libCrand_rng.';<br/>';          
+	
+?>
