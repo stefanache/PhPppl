a8904a8e51bb3ebd876e77fac37c7802880efca1
stefanache <ion.stefanache@yahoo.com>
2015-10-18 13:09:16 +0300

    Update util.php

--- util.php
+++ util.php
@@ -1,9 +1,20 @@
 <?php
- /*
- Mersenne Twister : A very fast random number generator Of period 219937-1
- */
-  $int_maxMTRand=mt_getrandmax();
-  function int_mtRandom($int_min=0,$int_max=$int_maxMTRand){
-    return mt_rand($int_min,$int_max);
+  /*
+  Mersenne Twister : A very fast random number generator Of period 219937-1
+  */
+  $class_MT = new stdClass();
+   $class_MT->int_maxMTRand = function int_maxMTRand(){
+                                return mt_getrandmax();
+                              }
+   
+    $class_MT->int_MTRand = function int_MTRand($int_min=0,$int_max=null){
+                              $int_min = (func_num_args() >= 1)? func_get_arg(0): 0;
+                              $int_max = (func_num_args() >= 2)? func_get_arg(1): int_maxMTRand();
+                              return mt_rand($int_min,$int_max);
+                            }
+   $class_MT->void_seedMTRand = function void_seedMTRand(){
+                                   $int_seed = (func_num_args() >= 1)? func_get_arg(0): null;
+                                   return ($int_seed)?mt_srand($int_seed):mt_srand();
+                                }
   }
 ?>
