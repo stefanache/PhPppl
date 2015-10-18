c7315e5f0da854c8679df6a3ec167c0eb18768d7
stefanache <ion.stefanache@yahoo.com>
2015-10-18 14:00:03 +0300

    Update Mersenne_Twister.php

--- Mersenne_Twister.php
+++ Mersenne_Twister.php
@@ -3,13 +3,17 @@
     /*
     Mersenne Twister : A very fast random number generator Of period 219937-1
     */
-  
+    public $intMT_rng;
+    public function __construct($int_min=0,$int_max=null, $int_seed=null){
+       $this->void_seedMTRand($int_seed);
+       $this->intMT_rng=$this->int_MTRand($int_min,$int_max);
+    }
     public function int_maxMTRand(){
       return mt_getrandmax();
     }
     public function int_MTRand($int_min=0,$int_max=null){
       $int_min = (func_num_args() >= 1)? func_get_arg(0): 0;
-      $int_max = (func_num_args() >= 2)? func_get_arg(1): int_maxMTRand();
+      $int_max = (func_num_args() >= 2)? func_get_arg(1): $this->int_maxMTRand();
       return mt_rand($int_min,$int_max);
     }
     public function float_makeSeed(){
@@ -17,7 +21,7 @@
       return (float) $sec + ((float) $usec * 100000);
     }
     public function void_seedMTRand(){
-      $int_seed = (func_num_args() >= 1)? func_get_arg(0): null;
+      $int_seed = (func_num_args() >= 1)? func_get_arg(0):$this->float_makeSeed();
       return ($int_seed)?mt_srand($int_seed):mt_srand();
     }
 
