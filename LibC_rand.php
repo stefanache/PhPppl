05f02a08de3addf1b3b75856d853b3fb3e817292
stefanache <ion.stefanache@yahoo.com>
2015-10-18 15:29:58 +0300

    Create LibC_rand.php

---
+++ LibC_rand.php
@@ -0,0 +1,48 @@
+<?php
+  class class_LibCrand{
+    /*
+       Mersenne Twister : Generate a random integer(no large) using libc
+                          in range [optional $int_min,optional $int_max] 
+                          with optional seed(initialization)
+    
+       usage: 
+          require_once('LibC_rand.php');
+          $libCrand_obj=new class_LibCrand();
+          $libCrand_rng=$libCrand_obj->intLibC_rng;
+          echo 'LibC rng = '.$libCrand_rng.';<br/>'; 
+    */
+    public $intLibC_rng=null;
+    
+    public function __construct($int_min=0,$int_max=null, $int_seed=null){
+      $int_seed=$this->int_makeSeed();
+      $this->void_seedLibCRand($int_seed);
+      $this->intLibC_rng=$this->int_LibCRand($int_min,$int_max);
+      return $this;
+    }
+    //
+    public function int_maxLibCrand(){
+      return getrandmax();
+    }
+    
+    public function int_LibCRand($int_min=0,$int_max=null){
+      $int_min = (func_num_args() >= 1)? func_get_arg(0): 0;
+      $intMAX=$this->int_maxLibCRand(); 
+      $int_max = (func_num_args() >= 2 && $int_max)? func_get_arg(1): $intMAX;
+      $int_max = min($int_max,$intMAX);
+      return rand($int_min,$int_max);
+    }
+    //
+    public function int_makeSeed(){
+      list($usec, $sec) = explode(' ', microtime());
+      $int_seed=(int) $sec + ((int) $usec * 100000);
+      $int_seed=min($int_seed,$this->int_maxLibCRand());
+      return $int_seed;
+    }
+    
+    public function void_seedLibCRand(){
+      $int_seed = (func_num_args() >= 1)? func_get_arg(0):null;
+      return ($int_seed) ? srand($int_seed) : srand();
+    }
+
+  }
+?>
