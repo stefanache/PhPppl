d77939dfcce829f394f4e7053570e2fc64a238fb
stefanache <ion.stefanache@yahoo.com>
2015-10-18 13:50:04 +0300

    Update Mersenne_Twister.php

--- Mersenne_Twister.php
+++ Mersenne_Twister.php
@@ -5,16 +5,20 @@
     */
   
     public function int_maxMTRand(){
-                      return mt_getrandmax();
+      return mt_getrandmax();
     }
     public function int_MTRand($int_min=0,$int_max=null){
-                      $int_min = (func_num_args() >= 1)? func_get_arg(0): 0;
-                      $int_max = (func_num_args() >= 2)? func_get_arg(1): int_maxMTRand();
-                      return mt_rand($int_min,$int_max);
+      $int_min = (func_num_args() >= 1)? func_get_arg(0): 0;
+      $int_max = (func_num_args() >= 2)? func_get_arg(1): int_maxMTRand();
+      return mt_rand($int_min,$int_max);
     }
-     public function void_seedMTRand(){
-                        $int_seed = (func_num_args() >= 1)? func_get_arg(0): null;
-                        return ($int_seed)?mt_srand($int_seed):mt_srand();
+    public function float_makeSeed(){
+      list($usec, $sec) = explode(' ', microtime());
+      return (float) $sec + ((float) $usec * 100000);
+    }
+    public function void_seedMTRand(){
+      $int_seed = (func_num_args() >= 1)? func_get_arg(0): null;
+      return ($int_seed)?mt_srand($int_seed):mt_srand();
     }
 
   }
