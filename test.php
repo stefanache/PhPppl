53894d5468dff14b5d966528e91b1c364c99f4d6
stefanache <ion.stefanache@yahoo.com>
2015-10-18 16:00:17 +0300

    Update test.php

--- test.php
+++ test.php
@@ -8,6 +8,13 @@
           require_once('LibC_rand.php');
           $libCrand_obj=new class_LibCrand();
           $libCrand_rng=$libCrand_obj->intLibC_rng;
-          echo 'LibC rng = '.$libCrand_rng.';<br/>';          
+          echo 'LibC rng = '.$libCrand_rng.';<br/>';
+          /*
+          //(PHP 7) support need!!!!
+          require_once('Crypt_Random.php');
+          $cr_obj=new class_CR();
+          $cr_rng=$cr_obj->intCR_rng;
+          echo 'Cryptographic secure preudo-rng = '.$cr_rng.';<br/>';
+          */                    
 	
 ?>
